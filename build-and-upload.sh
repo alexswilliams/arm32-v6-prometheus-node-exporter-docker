b756261131fa7b001deaba4431b505e971bd63d2
Alex Williams <lexw@live.co.uk>
2019-03-30 21:01:58 +0000

    Made pull conditional

--- build-and-upload.sh
+++ build-and-upload.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-set +ex
+set -ex
 
 function buildAndPush {
     local version=$1
-    docker build -t alexswilliams/arm32v6-prometheus-node-exporter:${version} --build-arg VERSION=${version} --file Dockerfile.arm32v6 .
-    docker push alexswilliams/arm32v6-prometheus-node-exporter:${version}
+    docker build -t alexswilliams/arm32v6-prometheus-node-exporter:${version} --build-arg VERSION=${version} --file Dockerfile.arm32v6 . \
+    && docker push alexswilliams/arm32v6-prometheus-node-exporter:${version}
 }
 
 buildAndPush "0.17.0"
