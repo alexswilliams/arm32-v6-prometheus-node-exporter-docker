c577de3b7a50f25e8b09a20f32df8a98487f56d2
Alex Williams <lexw@live.co.uk>
2019-03-31 00:18:22 +0000

    Bumped alpine version, installed binary into /bin instead of /app, wrapped with run.sh to support environment variables.

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+echo "Starting Prometheus Node Exporter $NODE_EXP_VERSION"
+echo "Relevant Environment Variables (NODE_EXP_*):"
+env | grep NODE_EXP
+
+/bin/node_exporter \
+    --collector.filesystem.ignored-mount-points=${NODE_EXP_COLLECTOR_FILESYSTEM_IGNORED_MOUNT_POINTS:-:^/(dev|proc|sys|var/lib/docker/.+)($|/)} \
+    --path.procfs=${NODE_EXP_PATH_PROCFS:-/proc} \
+    --path.sysfs=${NODE_EXP_PATH_PROCFS:-/sys} \
+    --path.rootfs=${NODE_EXP_PATH_PROCFS:-/} \
+    --web.listen-address=${NODE_EXP_WEB_LISTEN_ADDRESS:-:9100} \
+    $@
+
