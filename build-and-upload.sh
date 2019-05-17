5f9354e55f986f02f8e73c68315f44e3b1c1e2ed
Alex Williams <lexw@live.co.uk>
2019-05-17 20:00:12 +0100

    Added label-schema.org labels to docker images.

--- build-and-upload.sh
+++ build-and-upload.sh
@@ -4,10 +4,19 @@ set -ex
 
 function buildAndPush {
     local version=$1
-    docker build -t alexswilliams/arm32v6-prometheus-node-exporter:${version} --build-arg VERSION=${version} --file Dockerfile.arm32v6 . \
-    && docker push alexswilliams/arm32v6-prometheus-node-exporter:${version}
+    local imagename="alexswilliams/arm32v6-prometheus-node-exporter"
+    local fromline=$(grep -e '^FROM ' Dockerfile.arm32v6 | tail -n -1 | sed 's/^FROM[ \t]*//' | sed 's#.*/##' | sed 's/:/-/' | sed 's/#.*//' | sed -E 's/[ \t]+.*//')
+
+    docker build -t ${imagename}:${version} \
+        --build-arg VERSION=${version} \
+        --build-arg BUILD_DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ") \
+        --build-arg VCS_REF=$(git rev-parse --short HEAD) \
+        --file Dockerfile.arm32v6 . \
+    && docker tag ${imagename}:${version} ${imagename}:${version}-${fromline} \
+    && docker push ${imagename}:${version} \
+    && docker push ${imagename}:${version}-${fromline}
 }
 
-# buildAndPush "0.17.0"
+buildAndPush "0.17.0"
 buildAndPush "0.18.0"
 
